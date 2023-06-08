c942814823b3e37267b7921d17cf60493b513b28
karola-kasprzak <64924406+karola-kukowska@users.noreply.github.com>
2023-06-08 14:35:10 +0200

    changed icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useState } from "React";
+
+const url = "https://course-api.com/react-tours-project";
+
+useState(false);
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+    return <h2>Tours Starter.</h2>;
 };
 export default App;
---
+++ src/components/Tours.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Tours = () => {
+  useEffect(,[]);
+  
+    return <div>List</div>;
+};
+
+export default Tours;
